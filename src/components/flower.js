ce8b225b49666e2e071c2da0cca6be4e36e98ae7
Dasha <52924734+Daskendrik@users.noreply.github.com>
2021-01-30 18:33:14 +0300

    create windowGame and flower

---
+++ src/components/flower.js
@@ -0,0 +1,19 @@
+import './flower.css';
+
+function Flower() {
+
+  return (
+    <div className="Flower">
+      <div className="Petals">
+        <div className="Center"></div>
+      </div>
+      <div className="Bottom">
+        <div className="Stem"></div>
+        <div className="Leaf_1"></div>
+        <div className="Leaf_2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Flower;
